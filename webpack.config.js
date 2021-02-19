82f6f63ea7dca71d21dcd197f45f79db4488fd14
vmicic <vukasin.micic5@gmail.com>
2021-02-19 09:23:02 +0100

    Add external module

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const webpack = require('webpack')
+
+module.exports = {
+  target: 'webworker',
+  entry: './index.js',
+  mode: 'production',
+  plugins: [new webpack.ProvidePlugin({ _: 'lodash' })],
+}
