b38d794533bfdab0a78bca5f7197d66e49afad9a
vmicic <vukasin.micic5@gmail.com>
2021-02-25 10:01:06 +0100

    Add overlay config options

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
+const getOverlayPosition = (
+  config,
+  imageWidth,
+  imageHeight,
+  overlayWidth,
+  overlayHeight,
+) => {
+  let width = 0;
+  let height = 0;
+  if ('ot' in config || 'overlay_top' in config) {
+    height = config.ot ? +config.ot : +config.overlay_top;
+  }
+
+  if ('ob' in config || 'overlay_bottom' in config) {
+    const overlayBottom = config.ob ? +config.ob : +config.overlay_bottom;
+    height = imageHeight - overlayBottom - overlayHeight;
+  }
+
+  if ('ol' in config || 'overlay_left' in config) {
+    width = config.ol ? +config.ol : +config.overlay_left;
+  }
+
+  if ('or' in config || 'overlay_right' in config) {
+    const overlayRight = config.or ? +config.or : +config.overlay_right;
+    width = imageWidth - overlayRight - overlayWidth;
+  }
+  return { width, height };
+};
+
 const applyUrlConfig = async (urlConfig, base64Image) => {
   const configParts = urlConfig.split(',');
   const config = {};
@@ -21,12 +50,29 @@ const applyUrlConfig = async (urlConfig, base64Image) => {
     config[key] = value;
   });
 
-  console.log(JSON.stringify(config));
+  let base64OverlayImage = '';
+  if ('o' in config || 'overlay' in config) {
+    let uri = config.o || config.overlay;
+    uri = decodeURIComponent(uri);
+    const overlayImageResponse = await fetch(uri);
+
+    new Uint8Array(await overlayImageResponse.arrayBuffer()).forEach((byte) => {
+      base64OverlayImage += String.fromCharCode(byte);
+    });
+    base64OverlayImage = btoa(base64OverlayImage);
+  }
+
+  const overlayImage = await Jimp.read(
+    Buffer.from(base64OverlayImage, 'base64'),
+  );
+  const overylayWidth = overlayImage.bitmap.width;
+  const overlayHeight = overlayImage.bitmap.height;
+
   return Jimp.read(Buffer.from(base64Image, 'base64'))
     .then((image) => {
       if ('fit' in config) {
-        const requestWidth = +config.w || +config.width;
-        const requestHeight = +config.h || +config.height;
+        const requestWidth = config.w ? +config.w : +config.width;
+        const requestHeight = config.h ? +config.h : +config.height;
         if (config.fit === 'cover') {
           image.cover(requestWidth, requestHeight);
         }
@@ -61,7 +107,7 @@ const applyUrlConfig = async (urlConfig, base64Image) => {
         }
       } else {
         if ('w' in config || 'width' in config) {
-          const width = +config.w || +config.width;
+          const width = config.w ? +config.w : +config.width;
           if ('h' in config || 'height' in config) {
             const height = +config.h || +config.height;
             image.resize(width, height);
@@ -71,18 +117,39 @@ const applyUrlConfig = async (urlConfig, base64Image) => {
         }
 
         if ('h' in config || 'height' in config) {
-          const height = +config.h || +config.height;
+          const height = config.h ? +config.h : +config.height;
           image.resize(Jimp.AUTO, height);
         }
       }
       if ('q' in config || 'quality' in config) {
-        const quality = +config.q || +config.quality;
+        const quality = config.q ? +config.q : +config.quality;
         image.quality(quality);
       }
 
       if ('dpr' in config) {
         const ratio = +config.dpr;
         image.scale(ratio);
+      }
+
+      if ('o' in config || 'overlay' in config) {
+        const overlay = config.o ? +config.o : +config.overyaly;
+        if (overlay !== 'none') {
+          const options = {};
+          if ('oo' in config || 'overlay_opacity' in config) {
+            const overlayOpacity = config.oo
+              ? +config.oo
+              : +config.overlay_opacity;
+            options.opacitySource = overlayOpacity;
+          }
+          const { width, height } = getOverlayPosition(
+            config,
+            image.bitmap.width,
+            image.bitmap.height,
+            overylayWidth,
+            overlayHeight,
+          );
+          image.composite(overlayImage, width, height, options);
+        }
       }
 
       return image;
@@ -99,6 +166,16 @@ async function handleRequest(request) {
       accessKeyId: 'AKIA3SDE3YZN27ULMYYJ',
       secretAccessKey: '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',
     });
+
+    const overlayImageResponse = await fetch(
+      'https://p1.hiclipart.com/preview/318/447/663/alienware-invader-icons-small-sample-package-alienware-invader-icon-17-png-icon.jpg',
+    );
+
+    let base64OverlayImage = '';
+    new Uint8Array(await overlayImageResponse.arrayBuffer()).forEach((byte) => {
+      base64OverlayImage += String.fromCharCode(byte);
+    });
+    base64OverlayImage = btoa(base64OverlayImage);
 
     const imagePath =
       'https://vukasinsbucket.s3.eu-central-1.amazonaws.com/squarenumbers.jpg';
